1a23c456d3155c59605cdfbac9580898fb370c1e
Jmgfscvk <48419143+Jmgfscvk@users.noreply.github.com>
2019-12-04 15:25:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("##Successfully connected to MySQL container##");
 });
+
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+var path = __dirname + '/views/';
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+router.use(function(req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+router.get("/", function(req, res) {
+    res.sendFile(path + "index.html");
+});
+
+router.get("/sharks", function(req, res) {
+    res.sendFile(path + "sharks.html");
+});
